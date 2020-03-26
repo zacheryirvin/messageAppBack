c870cc37b345185f3ef7e6941b9fa1672b9c0bae
badCompany55 <zachirvin45@gmail.com>
2020-03-25 21:54:27 -0500

    list friends

--- database/actions/friendsActions.js
+++ database/actions/friendsActions.js
@@ -2,15 +2,6 @@ const query = require('../query.js')
 
 const friendsTb = {
   getFriends: async (userId) => {
-    // const friends = await query(`
-      // select friend_id
-      // from friends
-      // where user_id = '${userId}';
-    // `)
-    // const friendids = friends['rows'].map(x => {
-      // return x.friend_id;
-    // })
-    // console.log(friendids)
     return query(`
     select * from users
     where id in (select friend_id 

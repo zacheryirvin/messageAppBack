ce4990b2db17f047e995778c067a82d004b5dfd2
badCompany55 <zachirvin45@gmail.com>
2020-04-07 08:47:17 -0500

    add Message

--- database/actions/messageActions.js
+++ database/actions/messageActions.js
@@ -15,6 +15,7 @@ const messageTb = {
     insert into messages(from_id, to_id, message)
     values('${userId}', '${toId}', '${message}')
     `)
+    console.log(insert)
     return insert;
   },
   friendCheck: (userId, toId) => {

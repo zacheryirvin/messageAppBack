28629cb55078aa27ef689959314407b5451dc08b
badCompany55 <zachirvin45@gmail.com>
2020-04-07 22:00:32 -0500

    psql, all_users, triggers, date format

--- database/dbcreation.js
+++ database/dbcreation.js
@@ -18,6 +18,15 @@ const db = {
     const friends = await query(`
     drop table if exists friends 
     `)
+    const type = await query(`
+    drop type if exists user_friends cascade
+    `)
+    const triggerMessage = await query(`
+    drop type if exists triggerMessage
+    `)
+    const dropTrigger = await query(`
+    drop trigger watch_messages on messages
+    `)
     // console.log(friends)
   },
   createTables: async () => {
@@ -53,6 +62,59 @@ const db = {
           from_id uuid references users(id) not null
         )
         `)
+      const all_friends = await query(`
+      create or replace function all_friends(userId uuid)
+      returns setof user_friends
+      language plpgsql as $$
+      declare 
+      tempy cursor for 
+        select friend_id, pending, confirmed, requester
+        from friends
+        where user_id = userId;
+      return_val user_friends;
+      begin 
+        for i in tempy
+        loop
+          select id, first_name, last_name, user_name, email, i.pending, i.confirmed, i.requester
+          into return_val
+          from users 
+          where id = i.friend_id;
+          return next return_val;
+        end loop;
+      end; $$;
+      `)
+      const triggerMessage = await query(`
+      create type triggerMessage as (
+        id uuid,
+        from_id uuid,
+        to_id uuid,
+        time_stp text,
+        message text
+      )
+      `)
+      const trigger_function = await query(`
+      create or replace function notify_trigger()
+      returns trigger
+      as $$
+      declare
+      tempy triggerMessage;
+      begin 
+
+        tempy.id := new.id;
+        tempy.from_id := new.from_id;
+        tempy.to_id := new.to_id;
+        tempy.time_stp := to_char(new.time_stp, 'mm-dd-yy, hh24:mi:ss');
+        tempy.message := new.message;
+        perform pg_notify('watch_messages', row_to_json(tempy)::text);
+      --	new.time_stp := to_char(new.time_stp, 'mm-dd-yy, hh24::mi:ss');
+        return new;
+      end; $$ language plpgsql;
+      `)
+      const create_trigger = await query(`
+      create trigger watch_messages
+      after insert on messages
+      for each row execute procedure notify_trigger();
+      `)
       console.log('messages', createMessages);
     } catch(err) {
       console.log(err);

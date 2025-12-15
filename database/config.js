b926b6689231617990cb37a158e487ba270daca7
zachery irvin <zachirvin45@gmail.com>
2025-12-14 17:41:19 -0800

    postgres

--- database/config.js
+++ database/config.js
@@ -4,7 +4,7 @@ const { Pool } = require("pg");
 const isProduction = process.env.NODE_ENV === "production";
 
 const connectionString =
-  process.env.DATABASE_URI ||
+  process.env.DB_URI ||
   `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
